b09e1cf7153d1d1ff52031a7687fc28e37341729
Ghazal Shaikh <159722197+Ghazal010@users.noreply.github.com>
2024-03-10 21:12:19 +0500

    Add files via upload

---
+++ ghazal.ts
@@ -0,0 +1,9 @@
+/*Question 2: Personal Message: Store a person’s name in a variable 
+and print a message to them, like 
+“Hello Asharib, would you like to learn some TypeScript today?”*/
+
+let personName: string = "Ashraib";
+let message1: string = "would you like to learn some TypeScript today?";
+let greetings1: string = "Hello";
+
+console.log(greetings1 + " "  + personName + "," + message1);
